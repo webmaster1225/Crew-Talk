4932242315a21748365e5986e33b0725b05e53b4
Hardy Smith <hardysmith940110@gmail.com>
2022-06-12 14:16:36 -0500

    App clean up -- DuplicateRunner Error

--- app/src/main/java/dev/nguyen/crewtalk/Fragments/SearchFragment.kt
+++ app/src/main/java/dev/nguyen/crewtalk/Fragments/SearchFragment.kt
@@ -1,4 +1,4 @@
-package dev.nguyen.crewtalk.Fragments
+package dev.nguyen.crewtalk.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,11 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import dev.nguyen.crewtalk.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
 
     override fun onCreateView(
--- app/src/main/java/dev/nguyen/crewtalk/MainActivity.kt
+++ app/src/main/java/dev/nguyen/crewtalk/MainActivity.kt
@@ -1,12 +1,7 @@
 package dev.nguyen.crewtalk
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
@@ -15,14 +10,13 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
-import dev.nguyen.crewtalk.Fragments.ChatsFragment
-import dev.nguyen.crewtalk.Fragments.SearchFragment
-import dev.nguyen.crewtalk.Fragments.SettingsFragment
+import dev.nguyen.crewtalk.fragments.ChatsFragment
+import dev.nguyen.crewtalk.fragments.SearchFragment
+import dev.nguyen.crewtalk.fragments.SettingsFragment
 import dev.nguyen.crewtalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +31,6 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         // the "!!" will throw NullPointerException if the value is null
         supportActionBar!!.title = "" // this will help prevent showing default app's name in the action bar
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         val tabLayout: TabLayout = binding.tablayout
         val viewPager: ViewPager = binding.viewPager
@@ -70,21 +60,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
-
     internal class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
 
-        private val fragments: ArrayList<Fragment>
-        private val titles: ArrayList<String>
-
-        init {
-            fragments = ArrayList<Fragment>()
-            titles = ArrayList<String>()
-        }
+        private val fragments: ArrayList<Fragment> = ArrayList()
+        private val titles: ArrayList<String> = ArrayList()
 
         override fun getCount(): Int {
             return fragments.size
