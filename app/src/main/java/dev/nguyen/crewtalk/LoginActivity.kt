0c03fd5c0317ffc4b777934c857e08c34c9c8a51
Hardy Smith <hardysmith940110@gmail.com>
2022-06-12 17:03:42 -0500

    Setup back buttons on register and login activities

--- app/src/main/java/dev/nguyen/crewtalk/LoginActivity.kt
+++ app/src/main/java/dev/nguyen/crewtalk/LoginActivity.kt
@@ -1,11 +1,33 @@
 package dev.nguyen.crewtalk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import dev.nguyen.crewtalk.databinding.ActivityLoginBinding
+import dev.nguyen.crewtalk.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: Toolbar = binding.toolbarLogin
+        setSupportActionBar(toolbar)
+        // the "!!" will throw NullPointerException if the value is null
+        supportActionBar!!.title = "Login" // this will help prevent showing default app's name in the action bar
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // --> back button
+
+        // Click the back button
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this@LoginActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/dev/nguyen/crewtalk/RegisterActivity.kt
+++ app/src/main/java/dev/nguyen/crewtalk/RegisterActivity.kt
@@ -1,11 +1,31 @@
 package dev.nguyen.crewtalk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import dev.nguyen.crewtalk.databinding.ActivityMainBinding
+import dev.nguyen.crewtalk.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: Toolbar = binding.toolbarRegister
+        setSupportActionBar(toolbar)
+        // the "!!" will throw NullPointerException if the value is null
+        supportActionBar!!.title = "Register" // this will help prevent showing default app's name in the action bar
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // BACK BTN
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this@RegisterActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
