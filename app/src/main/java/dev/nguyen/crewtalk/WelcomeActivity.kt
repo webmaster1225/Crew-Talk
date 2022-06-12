58bbd0db8f07dc9a88e5120b79310073ce446ed0
Hardy Smith <hardysmith940110@gmail.com>
2022-06-12 16:43:53 -0500

    Add logics to register and login btns

--- app/src/main/java/dev/nguyen/crewtalk/WelcomeActivity.kt
+++ app/src/main/java/dev/nguyen/crewtalk/WelcomeActivity.kt
@@ -1,11 +1,47 @@
 package dev.nguyen.crewtalk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import dev.nguyen.crewtalk.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+    var firebaseUser: FirebaseUser? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerWelcomeBtn.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.loginWelcomeBtn.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        // USER VALIDATION
+        if (firebaseUser != null) {
+            val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish() //Call this when your activity is done and should be closed. The ActivityResult is propagated back to whoever launched you via onActivityResult()
+        }
+
     }
 }
