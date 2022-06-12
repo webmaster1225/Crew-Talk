7b56b4ca86b1899cae203b4f1e94ccecd82bf82a
Hardy Smith <hardysmith940110@gmail.com>
2022-06-12 13:18:41 -0500

    Created Search and Settings fragments

--- app/src/main/java/dev/nguyen/crewtalk/Fragments/SearchFragment.kt
+++ app/src/main/java/dev/nguyen/crewtalk/Fragments/SearchFragment.kt
@@ -9,15 +9,17 @@ import dev.nguyen.crewtalk.R
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ChatsFragment.newInstance] factory method to
+ * Use the [SearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ChatsFragment : Fragment() {
+class SearchFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
+
 }
--- app/src/main/java/dev/nguyen/crewtalk/Fragments/SettingsFragment.kt
+++ app/src/main/java/dev/nguyen/crewtalk/Fragments/SettingsFragment.kt
@@ -9,15 +9,17 @@ import dev.nguyen.crewtalk.R
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ChatsFragment.newInstance] factory method to
+ * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ChatsFragment : Fragment() {
+class SettingsFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
+
 }
