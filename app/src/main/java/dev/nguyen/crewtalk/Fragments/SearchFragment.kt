478f05f0da49323ae289485a9d57d8ecbc24696d
Hardy Smith <hardysmith940110@gmail.com>
2022-06-14 22:49:36 -0500

    Made search text box a live search

--- app/src/main/java/dev/nguyen/crewtalk/Fragments/SearchFragment.kt
+++ app/src/main/java/dev/nguyen/crewtalk/Fragments/SearchFragment.kt
@@ -1,12 +1,16 @@
 package dev.nguyen.crewtalk.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -15,22 +19,51 @@ import dev.nguyen.crewtalk.Adapters.UserAdapter
 import dev.nguyen.crewtalk.Models.Users
 import dev.nguyen.crewtalk.R
 
-class SearchFragment : Fragment() {
 
+class SearchFragment : Fragment() {
 
     private var userAdapter: UserAdapter? = null
     private var mUsers: List<Users>? = null
+    private var recyclerView: RecyclerView? = null
+    private var searchEditText: EditText? = null
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         // Inflate the layout for this fragment
         val view: View =  inflater.inflate(R.layout.fragment_search, container, false)
 
+        // declare Views
+        recyclerView = view.findViewById(R.id.searchList)
+        searchEditText = view.findViewById(R.id.searchUsersET)
         mUsers = ArrayList()
+
+        recyclerView!!.setHasFixedSize(true)
+        recyclerView!!.layoutManager = LinearLayoutManager(context)
+
+
         retrieveAllUser()
+
+        // make searchUsersET EditText view a live search
+        searchEditText!!.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(cs: CharSequence?, start: Int, before: Int, count: Int) {
+                searchForUser(cs.toString().toLowerCase())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
 
         return view
     }
@@ -53,19 +86,22 @@ class SearchFragment : Fragment() {
                 // Clear mUsers first
                 (mUsers as ArrayList<Users>).clear()
 
-                // for every user/snapshot in the DataSnapShot -> add every user to the mUsers arrayList
-                for (snapshot in p0.children) {
-                    // retrieve user's info from snapshot value
-                    val user: Users? = snapshot.getValue(Users::class.java)
+                if (searchEditText!!.text.toString().equals("")) {
+                    // for every user/snapshot in the DataSnapShot -> add every user to the mUsers arrayList
+                    for (snapshot in p0.children) {
+                        // retrieve user's info from snapshot value
+                        val user: Users? = snapshot.getValue(Users::class.java)
 
-                    // except your own account, add all the found results to the mUsers account array list
-                    if (!(user!!.getUid().equals(firebaseUserID))) {
-                        (mUsers as ArrayList<Users>).add(user)
+                        // except your own account, add all the found results to the mUsers account array list
+                        if (!(user!!.getUid().equals(firebaseUserID))) {
+                            (mUsers as ArrayList<Users>).add(user)
+                        }
                     }
-                }
 
-                // init userAdapter with given values
-                userAdapter = UserAdapter(context!!, mUsers!!, false)
+                    // init userAdapter with given values
+                    userAdapter = UserAdapter(context!!, mUsers!!, false)
+                    recyclerView!!.adapter = userAdapter
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -107,6 +143,7 @@ class SearchFragment : Fragment() {
 
                 // init userAdapter with given values
                 userAdapter = UserAdapter(context!!, mUsers!!, false)
+                recyclerView!!.adapter = userAdapter
             }
 
             override fun onCancelled(error: DatabaseError) {
