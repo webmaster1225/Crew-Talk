adf89fddc1987b5f732617e92124a521eb02dad7
Hardy Smith <hardysmith940110@gmail.com>
2022-06-21 09:35:45 -0500

    Cleaned up UserAdapter

--- app/src/main/java/dev/nguyen/crewtalk/adapters/UserAdapter.kt
+++ app/src/main/java/dev/nguyen/crewtalk/adapters/UserAdapter.kt
@@ -1,15 +1,18 @@
 package dev.nguyen.crewtalk.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import dev.nguyen.crewtalk.models.Users
 import dev.nguyen.crewtalk.R
+import dev.nguyen.crewtalk.activities.ChatActivity
 
 class UserAdapter (
     // "private val" added here help declare the variables -- no need to init the vars inside the class body
@@ -22,9 +25,6 @@ class UserAdapter (
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var userameTxt : TextView = view.findViewById(R.id.username)
         var profileImageView : CircleImageView = view.findViewById(R.id.profile_image)
-//        var onlineTxt : TextView = view.findViewById(R.id.image_online)
-//        var offlineTxt : TextView = view.findViewById(R.id.image_offline)
-//        var lastMessageTxt : TextView = view.findViewById(R.id.message_last)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
